2b3c7d553c2a11fb3601176457f0905ecf94d5e0
sayantanchatterjee <sayantan1992@live.com>
2020-06-11 14:12:22 +0530

    Modification for fetching default location from config/config.yml file if cusrrent location not found fix the file extension of a view

--- app/helpers/location_registers_helper.rb
+++ app/helpers/location_registers_helper.rb
@@ -1,2 +1,11 @@
 module LocationRegistersHelper
+    def self.get_default_coordinates
+        Rails.logger.info "Starting #{self}::#{__method__.to_s}"
+        coordinates = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]["default_coordinates"] rescue []
+        # Extra checking if any problem occurred during fetch data from config file or no data set in config
+        if !coordinates.present?
+            coordinates = [88.3627138688202,22.5827570785926]
+        end
+        return coordinates
+    end
 end
